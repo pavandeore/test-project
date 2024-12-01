ba90845a69c83e580b2dcb961149121e7c58fc63
pawan <pavandeore90@gmail.com>
2024-12-01 18:36:05 +0530

    added comments

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 let a = 3;
 let b = 5;
 console.log("Addition is: ", a + b);
+
+/*
+
+We do changes /  create new files []working area
+
+git add .  []staging area
+
+git commit -m "message" []local repository
+
+
+*/
