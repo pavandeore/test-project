c12de7536a8af5b549c9ecdb609613e64a8b3b84
pawan <pavandeore90@gmail.com>
2024-12-01 18:40:53 +0530

    added commented in main.js and created test.js

--- main.js
+++ main.js
@@ -11,4 +11,25 @@ git add .  []staging area
 git commit -m "message" []local repository
 
 
+
+
+on local machine you added addition 
+
+you will push these changes to remote [] github, gitlab, bitbucket
+
+
+Other guy  they will pull the changes from remote to their local machine
+
+
+and they will do changes and push to remote
+
+
+you will pull the changes from remote to your local machine
+
+
+you make changes and push again 
+
+
+
+
 */
